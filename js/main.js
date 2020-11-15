c1d49a465886bbde0920fbed9e44c1d911cdf254
apainintheneck <apainintheneck@gmail.com>
2020-11-15 22:02:41 +0000

    Added api call and the display of definitions and word usages.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 /*global $*/
+/*global fetch*/
 $(document).ready(function(){
+    const apiKey = "0a4a967d-fbcb-424d-9e52-b06b00ca52cb";
     
     //Event Listeners
     $("#enter-word").click(async function(){
@@ -7,6 +9,35 @@ $(document).ready(function(){
         
         if(!isWordValid(inputWord)) return;
         
+        let url = `https://www.dictionaryapi.com/api/v3/references/thesaurus/json/${inputWord}?key=${apiKey}`;
+        let response = await fetch(url);
+        let data = await response.json();
+        console.log(data);
+        
+        if(data.length < 1) {
+            $("#wordErrAlert").html("Unable to find word. Try another one.");
+            return;
+        }
+        
+        $("#word-title").html(data[0].meta.id.toUpperCase());
+        
+        for(let i = 0; i < data.length; i++){
+            if(data[i].meta.id === inputWord) {
+                $("#word-usage").append(`<option value="${i}"> ${data[i].fl}`);
+            }
+        }
+        $("#word-usage").append(`hr>`);
+        
+        
+        $("#definition").html("");
+        
+        for(let i = 0; i < data[0].shortdef.length; i++) {
+            $("#definition").append(`<option value="${i}"> ${data[0].shortdef[i]}`);
+        }
+        
+        //Add synonyms 
+        
+        //Add antonyms
         
     });
     
