fcc59d3ac6b860f5bbb00c515ba6e03118ae49ae
apainintheneck <apainintheneck@gmail.com>
2020-11-15 19:03:24 +0000

    Fixed button click bug.

--- js/main.js
+++ js/main.js
@@ -2,27 +2,27 @@
 $(document).ready(function(){
     
     //Event Listeners
-    $("#enter-word").click(getWordInfo());
-    $("#input-word").on("change", isWordValid($("#input-word").val()))
-    
-    //Functions
-    async function getWordInfo(){
+    $("#enter-word").click(async function(){
         let inputWord = $("#input-word").val();
         
         if(!isWordValid(inputWord)) return;
         
         
-    }
+    });
     
+    //Functions
     function isWordValid(word) {
-        if(!isNaN(word)) {
-            $("#wordErrAlert").html("Enter a valid word.");
-        } else {
-            $("#wordErrAlert").html("");
+        if(word !== "") {
+            for(let i = 0; i < word.length; i++) {
+                if(word[i] >= "0" && word[i] <= "9") {
+                    $("#wordErrAlert").html("Enter a valid word.");
+                    return false;
+                }
+            }
         }
-    }
-    
-    function displayWordCloud(htmlId, data) {
         
+        $("#wordErrAlert").html("");
+        return true;
     }
+
 });
